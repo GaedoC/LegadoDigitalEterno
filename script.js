017d9b7b3e3a5faf067ad6f42846b726a5368f16
gaedo.vrgroup <gaedo.vrgroup@bicevida.cl>
2024-08-06 01:53:12 -0400

    3 up

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ document.querySelector('form').addEventListener('submit', function(event) {
     console.log('Message:', message);
 
     alert('Gracias por tu mensaje. Te contactaremos pronto.');
+
+
+
+
+
+
+});
+
+document.getElementById('scheduleForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevenir el envío tradicional del formulario
+
+    const fecha = document.getElementById('fecha').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+    // Aquí puedes añadir la lógica para enviar la información al servidor o manejarla como necesites
+    console.log('Fecha de publicación:', fecha);
+    console.log('Mensaje:', mensaje);
+
+    // Mostrar confirmación al usuario
+    document.getElementById('confirmacion').innerHTML = `Tu mensaje ha sido programado para ser publicado el ${fecha}.`;
 });
